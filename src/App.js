c949c10b7cd4df52e0465b8e839695c440ea700c
zak258 <zakaria.k.hamdan@gmail.com>
2020-07-09 15:25:03 +0200

    playing different notes on click

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Fragment, useState } from "react";
 import Zsynth from "./components/Zsynth";
+import notes from "./notes.json";
 
 const App = () => {
+  const [zNotes, setZnotes] = useState(notes);
   const [zOsc, setZosc] = useState("triangle");
   const [zEnv, setZenv] = useState({
-    attack: 0.005,
+    attack: 0.1,
     decay: 0.1,
     sustain: 0.3,
     release: 1,
   });
   const [zVol, setZvol] = useState(-1);
+
   return (
     <Fragment>
       <h1>Zamagana</h1>
-      <Zsynth zOsc={zOsc} zEnv={zEnv} zVol={zVol} />
+      <Zsynth zOsc={zOsc} zEnv={zEnv} zVol={zVol} zNotes={zNotes} />
     </Fragment>
   );
 };
