6b5148e8bb14cf9a13c17a3e4503320e8e78a9d7
NLJZ <johnson.nathan.lee@gmail.com>
2020-07-09 16:40:34 +0200

    tiny changes

--- src/components/Zsynth.js
+++ src/components/Zsynth.js
@@ -1,13 +1,13 @@
 import React, { Fragment, Component } from "react";
 import * as Tone from "tone";
 
-import Keys from "./Zkey";
+import Zkeys from "./Zkey";
 
 const Zsynth = (props) => {
   console.log(props);
   var zsynth = new Tone.Synth().toMaster();
 
-  return <Keys zNotes={props.zNotes} zsynth={zsynth} />;
+  return <Zkeys zNotes={props.zNotes} zsynth={zsynth} />;
 };
 
 export default Zsynth;
