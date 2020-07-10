12652e3b92fcf6233e7cddbef769519c698776d9
zak258 <zakaria.k.hamdan@gmail.com>
2020-07-10 15:53:24 +0200

    change octave button added

--- src/components/Zkeys.js
+++ src/components/Zkeys.js
@@ -1,52 +1,85 @@
-import React from "react";
+import React, { Component } from "react";
 import * as Tone from "tone";
 import KeyboardEventHandler from "react-keyboard-event-handler";
 
-const Zkeys = (props) => {
-  function play(note) {
-    Tone.start();
-    props.zTone.triggerAttackRelease(note, "5n");
+// const Zkeys = (props) => {
+
+//   return (
+
+//   );
+// };
+
+// export default Zkeys;
+
+// play(key.note+key.octave)
+
+class Zkeys extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { octave: 4 };
   }
 
-  return (
-    <div className="set">
-      <ul>
-        {props.zNotes.map((key) => (
-          <div
-            className={key.color}
-            id={key.id}
-            key={key.id}
-            onClick={() => play(key.note)}
-          >
-            {key.keyboardKey}
-          </div>
-        ))}
-      </ul>
-      <KeyboardEventHandler
-        handleKeys={[
-          "a",
-          "w",
-          "d",
-          "r",
-          "f",
-          "g",
-          "y",
-          "h",
-          "u",
-          "j",
-          "i",
-          "k",
-        ]}
-        onKeyEvent={(key, e) =>
-          props.zNotes.map((obj) => {
-            if (key === obj.keyboardKey) {
-              play(obj.note);
-            }
-          })
-        }
-      />
-    </div>
-  );
-};
+  play = (note) => {
+    Tone.start();
+    this.props.zTone.triggerAttackRelease(note + this.state.octave, "0.1s");
+  };
+
+  handleIncrement = () => {
+    const oct = this.state.octave + 1;
+    this.setState({ octave: oct });
+  };
+
+  handleDecrement = () => {
+    const oct = this.state.octave - 1;
+    this.setState({ octave: oct });
+  };
+
+  render() {
+    return (
+      <div className="set">
+        <ul>
+          {this.props.zNotes.map((key) => (
+            <div
+              className={key.color}
+              id={key.id}
+              key={key.id}
+              onClick={() => this.play(key.note)}
+            >
+              {key.keyboardKey}
+            </div>
+          ))}
+        </ul>
+        <KeyboardEventHandler
+          handleKeys={[
+            "a",
+            "w",
+            "d",
+            "r",
+            "f",
+            "g",
+            "y",
+            "h",
+            "u",
+            "j",
+            "i",
+            "k",
+          ]}
+          onKeyEvent={(key, e) =>
+            this.props.zNotes.map((obj) => {
+              if (key === obj.keyboardKey) {
+                this.play(obj.note);
+              }
+            })
+          }
+        />
+        <ul className="octave">
+          <li onClick={this.handleIncrement}>+</li>
+          <li>{`octave ${this.state.octave}`}</li>
+          <li onClick={this.handleDecrement}>-</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Zkeys;
--- src/components/Zsynth.js
+++ src/components/Zsynth.js
@@ -3,7 +3,7 @@ import * as Tone from "tone";
 import Zkeys from "./Zkeys";
 
 const Zsynth = (props) => {
-  var zTone = new Tone.Synth().toMaster();
+  var zTone = new Tone.PolySynth().toMaster();
   return (
     <Fragment>
       <Zkeys zNotes={props.zNotes} zTone={zTone} />
