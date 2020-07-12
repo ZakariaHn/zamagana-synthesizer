40d9ac2171423170004b9f1f0ab684948732817e
NLJZ <johnson.nathan.lee@gmail.com>
2020-07-12 11:20:59 +0200

    trying to fix issues

--- src/components/Zsynth.js
+++ src/components/Zsynth.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as Tone from "tone";
-import Zkeys from "./Zkey";
+import Zkeys from "./Zkeys";
 import Zcontrols from "./Zcontrols";
 
 const Zsynth = (props) => {
