882b98abd42eb32f392de2d97feebd4b05e518e9
zak258 <zakaria.k.hamdan@gmail.com>
2020-07-14 15:23:11 +0200

    change buttons to sliders

--- src/components/controlers/ZOctaveController.js
+++ src/components/controlers/ZOctaveController.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 const ZOctaveController = (props) => {
+  const { zOct, setZoct } = props;
+  const handaleOctave = (e) => {
+    setZoct(e.target.value);
+  };
   return (
-    <div className="octave control">
-      <ul>
-        <li onClick={() => props.setZoct((x) => x + 1)}>+</li>
-        <li>{`Octave ${props.zOct}`}</li>
-        <li onClick={() => props.setZoct((x) => x - 1)}>-</li>
-      </ul>
-    </div>
+    <Fragment>
+      <div className="octaveSliderTiltle">Octave</div>
+      <div className="octave control">
+        <input
+          type="range"
+          min={1}
+          max={3}
+          value={zOct}
+          onChange={handaleOctave}
+        />
+      </div>
+    </Fragment>
   );
 };
 
