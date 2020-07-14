3f56f7ce7ddd63ee860762a70882a134a03854da
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-07-14 13:12:27 +0200

    ZreverbController feature update

--- src/components/controlers/ZreverbController.js
+++ src/components/controlers/ZreverbController.js
@@ -1,11 +1,47 @@
 import React from "react";
 const ZreverbController = (props) => {
   return (
-    <div className="reverb control">
-      <ul>
-        <li onClick={() => props.setZrev((x) => x + 0.1)}>+</li>
-        <li>Reverb</li>
-        <li onClick={() => props.setZrev((x) => x - 0.1)}>-</li>
+    <div className="reverb-control">
+      <ul className="reverb-decay">
+        <li
+          onClick={() =>
+            props.setZrev({ ...props.zRev, decay: props.zRev.decay + 0.5 })
+          }
+        >
+          +
+        </li>
+        <li>decay{props.zRev.decay}</li>
+        <li
+          onClick={() =>
+            props.setZrev({ ...props.zRev, decay: props.zRev.decay - 0.5 })
+          }
+        >
+          -
+        </li>
+      </ul>
+
+      <ul className="reverb-preDelay">
+        <li
+          onClick={() =>
+            props.setZrev({
+              ...props.zRev,
+              preDelay: props.zRev.preDelay + 0.01,
+            })
+          }
+        >
+          +
+        </li>
+        <li>preDelay{props.zRev.preDelay}</li>
+        <li
+          onClick={() =>
+            props.setZrev({
+              ...props.zRev,
+              preDelay: props.zRev.preDelay - 0.01,
+            })
+          }
+        >
+          -
+        </li>
       </ul>
     </div>
   );
