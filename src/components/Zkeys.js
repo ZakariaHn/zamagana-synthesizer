2df77b748edca6c9bacc14fa10e49f95c4feb18e
zak258 <zakaria.k.hamdan@gmail.com>
2020-07-16 13:53:13 +0200

    set parameters

--- src/components/Zkeys.js
+++ src/components/Zkeys.js
@@ -18,7 +18,7 @@ const Zkeys = (props) => {
     and rendering the synth keys and setting it's class based on the data entered there.
  */
 
-  let style = { color: "red" };
+  let style = { color: "red", fontWeight: "bold" };
   const CminorScale = ["C", "D", "Eb", "F", "G", "Ab", "Bb"];
   const renderSynthKeys = zNotes.map((key) => (
     <div
@@ -26,7 +26,7 @@ const Zkeys = (props) => {
       id={key.id}
       key={key.id}
       onMouseDown={() => play(key.note)}
-      // style={CminorScale.includes(key.note) ? style : key}
+      style={CminorScale.includes(key.note) ? style : key}
     >
       {key.keyboardKey}
     </div>
