5e40f12c7365e61b0483dcca911fed76d26c6fe0
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-07-16 14:14:16 +0200

    Updates

---
+++ precache-manifest.1c4eebb574d6ac94bd19a9149cb0839a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b0d4f3e9b2041aefd37d4465b91f25",
+    "url": "/gabrielaacha/zamagana_synth/index.html"
+  },
+  {
+    "revision": "cd509c49779272805e99",
+    "url": "/gabrielaacha/zamagana_synth/static/css/main.c7388637.chunk.css"
+  },
+  {
+    "revision": "c87230248ae9d1086bd2",
+    "url": "/gabrielaacha/zamagana_synth/static/js/2.934103c8.chunk.js"
+  },
+  {
+    "revision": "b28590a4d5a036be9947256ff9f1f35b",
+    "url": "/gabrielaacha/zamagana_synth/static/js/2.934103c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd509c49779272805e99",
+    "url": "/gabrielaacha/zamagana_synth/static/js/main.77ac9ddd.chunk.js"
+  },
+  {
+    "revision": "94e7baa2ca2519256100",
+    "url": "/gabrielaacha/zamagana_synth/static/js/runtime-main.4cfe1990.js"
+  }
+]);
